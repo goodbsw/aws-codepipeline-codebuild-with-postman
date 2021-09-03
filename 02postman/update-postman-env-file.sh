9e0c6a217b453f7edefc71d54e5ec13e48d24b9c
Seungweon Baek <goodbsw@gmail.com>
2021-09-03 15:39:25 -0700

    WIP

--- 02postman/update-postman-env-file.sh
+++ 02postman/update-postman-env-file.sh
@@ -2,6 +2,7 @@
 
 #This shell script updates Postman environment file with the API Gateway URL created
 # via the api gateway deployment
+# how this works when create a stack?
 
 echo "Running update-postman-env-file.sh"
 
